0b54adcb159535c6a2ba61aaa985866ea61430b8
Paul Dixon <paul.dixon@gmail.com>
2016-07-01 18:55:25 +0100

    Changes to use docker

--- build.sh
+++ build.sh
@@ -1,4 +1,15 @@
-#!/bin/bash -e
+#!/bin/bash
+set -e
+
+# If running as root (e.g. under Docker)
+if [[ $EUID -eq 0 ]]
+then
+  SUDO=
+  RVM_SCRIPT=/etc/profile.d/rvm.sh
+else
+  SUDO=sudo
+  RVM_SCRIPT=$HOME/.rvm/scripts/rvm
+fi
 
 # Build an HAProxy RPM with ssl and lua support
 
@@ -12,41 +23,43 @@ INSTALL_ROOT="${INSTALL_ROOT:-${WORKSPACE}/oss}"
 BUILD_ROOT="${BUILD_ROOT:-${WORKSPACE}/build}"
 HAPROXY_VERSION="${HAPROXY_VERSION:-1.6.6}"
 LUA_VERSION="${LUA_VERSION:-5.3.3}"
+RVM_VERSION="${RVM_VERSION:-2.2.1}"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 REDHAT_VERSION=$(awk '{print substr ($3, 0, 1)}' /etc/redhat-release)
 
 # Install development tools and libraries
-sudo yum -y groupinstall "Development tools"  
-sudo yum -y install openssl-devel pcre-devel zlib-devel readline-devel libtermcap-devel
+$SUDO yum -y groupinstall "Development tools"  
+$SUDO yum -y install openssl-devel pcre-devel zlib-devel readline-devel libtermcap-devel wget curl
 
 # Install fpm (this is used to build the RPM)
-if [ ! -f $HOME/.rvm/gems/ruby-1.9.3-p551/bin/fpm ]
+if [ ! -f $RVM_SCRIPT ]
 then
-
   if [ $REDHAT_VERSION -gt 5 ]
   then
     gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
   else
-    sudo yum -y install gpg
+    $SUDO yum -y install gpg
     gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
   fi
 
-  if [ ! -f $HOME/.rvm/VERSION ]
-  then
-    \curl -sSL https://get.rvm.io | bash -s -- --ignore-dotfiles
-  fi
-  export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
-  [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
-  rvm install 1.9.3
-  rvm use 1.9.3
+  \curl -sSL https://get.rvm.io | bash -s -- --ignore-dotfiles
+  
+  # set +e before sourceing the rvm script as it has none zero exits in the
+  # script somewhere (presumably a bug).
+  set +e
+  source $RVM_SCRIPT
+  rvm install $RVM_VERSION
+  rvm use $RVM_VERSION
+  set -e
   gem install fpm
 else
-  export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
-  [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
-  rvm use 1.9.3
+  set +e
+  source $RVM_SCRIPT
+  rvm use $RVM_VERSION
+  set -e
 fi
-
+set -e
 
 # Create empty INSTALL_ROOT - this is where we'll make install to.
 if [ -d $INSTALL_ROOT ]
@@ -62,12 +75,15 @@ then
 fi
 mkdir $BUILD_ROOT
 
+# Due to a bug in rvm, running `cd` results in a none zero exit.
+# So from now on, we use `builtin cd`
+
 # Build LUA
 LUA_SRC="lua-${LUA_VERSION}"
-cd $BUILD_ROOT
+builtin cd $BUILD_ROOT
 wget http://www.lua.org/ftp/${LUA_SRC}.tar.gz
 tar xzf $LUA_SRC.tar.gz
-cd $LUA_SRC
+builtin cd $LUA_SRC
 
 # Add -ltermcap to the Makefile on EL5 variants to fix
 # undefined references in libreadline.so
@@ -83,12 +99,12 @@ make linux
 make INSTALL_TOP=$INSTALL_ROOT/$LUA_SRC install
 
 # Build HAProxy
-cd $BUILD_ROOT
+builtin cd $BUILD_ROOT
 HAPROXY_BRANCH=${HAPROXY_VERSION%.*}
 HAPROXY_SRC="haproxy-${HAPROXY_VERSION}"
 wget http://www.haproxy.org/download/$HAPROXY_BRANCH/src/$HAPROXY_SRC.tar.gz
 tar xzf $HAPROXY_SRC.tar.gz
-cd $HAPROXY_SRC
+builtin cd $HAPROXY_SRC
 
 LUA_ROOT="$INSTALL_ROOT/$LUA_SRC"
 if [ $REDHAT_VERSION -eq 5 ]
@@ -111,7 +127,7 @@ then
   chmod 755 $INSTALL_ROOT/haproxy/etc/rc.d/init.d/haproxy
 fi
 
-cd $WORKSPACE
+builtin cd $WORKSPACE
 rm -f *.rpm
 fpm -s dir -t rpm -n haproxy -v $HAPROXY_VERSION --config-files /etc/haproxy/ -C ~/oss/haproxy/ usr etc var
 [ ! -d /vagrant/rpms/el${REDHAT_VERSION} ] && mkdir -p /vagrant/rpms/el${REDHAT_VERSION}
